21c05174a9ae46fbafd6638c152cb09afcf767f2
kmj0505 <menge0505@naver.com>
2023-05-10 22:39:01 +0900

    230510_Study

---
+++ Level_1/230510_없는 숫자 더하기.cpp
@@ -0,0 +1,35 @@
+// 없는 숫자 더하기
+
+// 문제 설명 : 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다. 
+//			   numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+
+// ex) numbers = [1, 2, 3, 4, 6, 7, 8, 0] result = 14
+//	   numbers = [5, 8, 4, 0, 6, 7, 9] result = 6
+
+
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    vector<int> numbers = { 1,2,3,4,6,7,8,0 };
+    int answer = -1;
+    int sum = 0;
+    int num = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        sum += i;
+    }
+    for (int j = 0; j < numbers.size(); j++)
+    {
+        num += numbers[j];
+    }
+    if (sum != num)
+    {
+        answer = sum - num;
+    }
+    cout << answer;
+}
